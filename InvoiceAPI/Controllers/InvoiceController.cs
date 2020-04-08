85d6ebd020e35d8a9e2b1d517e27ae93946b8e14
saifkbr <saif_kbr@yahoo.co.in>
2020-04-08 16:06:20 +0100

    Initial commit

---
+++ InvoiceAPI/Controllers/InvoiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InvoiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        private readonly ILogger<InvoiceController> _logger;
+
+        public InvoiceController()
+        {
+            ILogger<InvoiceController> logger)
+        {
+                _logger = logger;
+            }
+        }
+    }
+}
