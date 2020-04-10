5e8a65384acd6b1423d121af5ff5cb409c50cfaa
saifkbr <saif_kbr@yahoo.co.in>
2020-04-10 17:56:12 +0100

    Added NewtonsoftJson for datatype auto conversion

--- InvoiceAPI/Startup.cs
+++ InvoiceAPI/Startup.cs
@@ -31,6 +31,7 @@ namespace InvoiceAPI
                 Configuration.GetConnectionString("SqliteConnection")
             ));
             services.AddScoped<IRepository, SqliteRepository>();
+                        
             services.AddCors(c =>
             c.AddPolicy("AllowOrigin",
             o =>
@@ -40,7 +41,7 @@ namespace InvoiceAPI
             )
             );
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
